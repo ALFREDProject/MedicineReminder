76179c3e162a1d0d3d453f2322caef012530fcd4
Robert Konrad <robert@ktx-software.com>
2016-02-16 14:52:01 +0100

    Show circle button.

--- app/src/main/java/eu/alfred/medicinereminder/MainActivity.java
+++ app/src/main/java/eu/alfred/medicinereminder/MainActivity.java
@@ -1,59 +1,24 @@
 package eu.alfred.medicinereminder;
 
-import android.app.AlertDialog;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.ServiceConnection;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.media.AudioManager;
-import android.os.AsyncTask;
-import android.os.Handler;
-import android.os.IBinder;
-import android.os.Message;
-import android.os.Messenger;
-import android.os.RemoteException;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.TaskStackBuilder;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Base64;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.util.Log;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.Arrays;
 import java.util.Map;
 
-import eu.alfred.api.PersonalAssistant;
-import eu.alfred.api.PersonalAssistantConnection;
-import eu.alfred.api.sensors.SAFFacade;
-import eu.alfred.api.sensors.responses.SensorDataResponse;
-import eu.alfred.api.speech.Cade;
-import eu.alfred.api.speech.responses.CadeResponse;
-import eu.alfred.api.storage.CloudStorage;
-import eu.alfred.api.storage.responses.BucketResponse;
-
+import eu.alfred.ui.CircleButton;
 
 public class MainActivity extends eu.alfred.ui.AppActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        circleButton = (CircleButton)findViewById(R.id.voiceControlBtn);
+        circleButton.setOnTouchListener(new CircleTouchListener());
     }
 
     @Override
     public void performAction(String command, Map<String, String> map) {
-
+        Log.i("medicine", "Perform action " + command);
     }
 }
